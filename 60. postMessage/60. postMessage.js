79cec9fdc1ddf411b74bdeb1112598d7dfa3585d
Vidhya Sagar Thakur <vsthakur.it0@gmail.com>
2024-12-16 17:56:59 +0530

    postMessage browser API

---
+++ 60. postMessage/60. postMessage.js
@@ -0,0 +1,21 @@
+console.log(1)
+
+window.onmessage = () => {
+  console.log(2)
+}
+
+Promise.resolve().then(() => {
+  console.log(3)
+})
+
+setTimeout(() => {
+  console.log(4)
+}, 0)
+
+console.log(5)
+
+window.postMessage('')
+
+console.log(6)
+
+// 1, 5, 6, 3, 2, 4
